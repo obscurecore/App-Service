93f60d2c10bec1fcfc31c779f988d3f4315a4d7b
Ruslan Potapov <pruslang@gmail.com>
2020-05-09 18:24:18 +0300

    refactor and add comments

--- eureka-server/src/main/java/ru/ruslan/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ru/ruslan/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Eureka Server is implementation of client-side discovery pattern
+ * Thus, each service registers with Eureka and sends an echo request to the Eureka server to inform it that it is active
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {

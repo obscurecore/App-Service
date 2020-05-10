e86b6db342d1a458a131ff780aa3b824626c00a1
Ruslan Potapov <pruslang@gmail.com>
2020-05-10 13:18:13 +0300

    add annotation (enable service) for user-service

--- user-service/src/main/java/ru/ruslan/userservice/UserServiceApplication.java
+++ user-service/src/main/java/ru/ruslan/userservice/UserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
